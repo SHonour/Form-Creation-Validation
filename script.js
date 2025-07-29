b6150993b07029cac070b1a20129ef918ac634ee
Honour Shalom <shalomhonour1@gmail.com>
2025-07-29 18:13:24 +0100

    added the script.js file

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("registration-form")
+    const feedbackDiv = document.getElementById("form-feedback")
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+    })
+    const username = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    let isValid = true;
+    const messages = [];
+
+    if (username.length < 3) { isValid = false; 
+    messages.push("Username must be at least 3 character long")   
+    }   
+
+    console.log(messages)
+    if (!email.includes("@") && !email.includes(".")) {
+        isValid = false;
+        messages.push("This is not a valid email")
+    }   
+    
+    if (password.length < 8) { isValid = false; 
+    messages.push("password is too short")   
+    }
+    
+    feedbackDiv.style = "block";
+    if( isValid = true) {
+        feedbackDiv.textContent = "Registration Successful";
+        feedbackDiv.style.color ="#28a745";
+    } else { 
+        feedbackDiv.innerHTML = messages.join("<br>");
+        feedbackDiv.style.color = "#dc3545"
+
+    }
+    
 })
